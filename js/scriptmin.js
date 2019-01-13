9f48a1383b6e31d2e5914a2d95ecb419fca2bee2
Nickdem <45073853+Nickdem@users.noreply.github.com>
2019-01-13 20:38:15 +0300

    защита 2

---
+++ js/scriptmin.js
@@ -0,0 +1,1 @@
+var btn=document.querySelector(".search-hotel-button"),popup=document.querySelector(".search-hotel-popup-wrapper");btn.addEventListener("click",function(){popup.classList.toggle("show")}),document.addEventListener("DOMContentLoaded",function(){popup.classList.remove("show")});
